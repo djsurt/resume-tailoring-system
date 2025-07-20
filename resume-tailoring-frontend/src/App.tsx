7cb514bbdee25f81d40194e28cd8d1337e727863
Dhananjay Surti <djsurti3003@gmail.com>
2025-07-20 18:14:49 +0400

    Get fronttend connected with the backend api

--- resume-tailoring-frontend/src/App.tsx
+++ resume-tailoring-frontend/src/App.tsx
@@ -9,10 +9,46 @@ function App() {
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setOutput('')
+    setError(null)
+    setLoading(true)
+    console.log("Submitting")
+    console.log(loading)
+    const formData = new FormData()
+    formData.append('job_url', jobUrl)
+    if(resumeFile){
+      formData.append('resume', resumeFile)
+    }
+
+    try{
+      const response = await fetch('http://127.0.0.1:8000/analyze/', {
+        method: 'POST',
+        body: formData,
+      })
+
+      if(!response.body) throw new Error('No response body')
+      const reader = response.body.getReader()
+      const decoder = new TextDecoder()
+      let done = false
+
+      while(!done){
+        const { value, done: doneReading } = await reader.read()
+        done = doneReading
+        if(value) setOutput((prev) => prev + decoder.decode(value))
+      }
+    } catch(err: any){
+      setError(err.nessage)
+    } finally{
+      setLoading(false)
+    }
+  }
+
   return (
     <div className='container'>
       <h1>Resume Tailoring System</h1>
-      <form onSubmit={() => {}} className='form'>
+      <form onSubmit={handleSubmit} className='form'>
         <input
         type='url'
         placeholder="Job Posting URL"
